88f870d14b0e18d5be5fdbbf83451a400022d09a
sunchulbaek <sunchulbaek@ncsoft.com>
2022-05-20 18:15:16 +0900

    검색어 입력 영역 생성

--- app/src/main/java/kr/pe/ssun/composedemo/MainActivity.kt
+++ app/src/main/java/kr/pe/ssun/composedemo/MainActivity.kt
@@ -3,41 +3,48 @@ package kr.pe.ssun.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import kr.pe.ssun.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+                    val (button, text) = createRefs()
+                    Button(onClick = {},
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(60.dp)
+                            .constrainAs(button) {
+                                top.linkTo(parent.top)
+                                end.linkTo(parent.end)
+                            }) {
+                        Text("Search")
+                    }
+
+                    var textState by remember { mutableStateOf(TextFieldValue("")) }
+                    TextField(
+                        value = textState,
+                        onValueChange = { textState = it },
+                        modifier = Modifier.constrainAs(text) {
+                            top.linkTo(button.top)
+                            bottom.linkTo(button.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(button.start)
+                        }
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeDemoTheme {
-        Greeting("Android")
     }
 }
