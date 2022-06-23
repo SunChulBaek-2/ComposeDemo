6becadc2dea3958d4959a91d43832ae70db713e4
sunchulbaek <sunchulbaek@ncsoft.com>
2022-06-23 15:22:02 +0900

    라이브러리 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.2.0" apply false
-    id("com.android.library") version "7.2.0" apply false
+    id("com.android.application") version "7.2.1" apply false
+    id("com.android.library") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version Versions.KOTLIN apply false
     id("com.github.ben-manes.versions") version "0.42.0" apply true
     id("org.jetbrains.kotlin.jvm") version Versions.KOTLIN apply false
