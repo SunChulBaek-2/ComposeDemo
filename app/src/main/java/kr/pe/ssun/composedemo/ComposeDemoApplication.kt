b30beab8a2d665fdf98ce463ceeb868d1061598f
sunchulbaek <sunchulbaek@ncsoft.com>
2022-06-27 16:08:01 +0900

    coil disk cache 설정

--- app/src/main/java/kr/pe/ssun/composedemo/ComposeDemoApplication.kt
+++ app/src/main/java/kr/pe/ssun/composedemo/ComposeDemoApplication.kt
@@ -1,6 +1,9 @@
 package kr.pe.ssun.composedemo
 
 import android.app.Application
+import coil.Coil
+import coil.ImageLoader
+import coil.disk.DiskCache
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,6 +12,19 @@ class ComposeDemoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Coil disk cache 설정
+        Coil.setImageLoader(
+            ImageLoader.Builder(applicationContext)
+                .diskCache {
+                    DiskCache.Builder()
+                        .directory(applicationContext.cacheDir.resolve("image_cache"))
+                        .build()
+                }
+                .build()
+        )
+
+        // Timber 설정
         Timber.plant(Timber.DebugTree())
     }
 }
